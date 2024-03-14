52dd908fffbe6c49508c4d965e2398c7c1c11aa9
wee-owl <kem-cv@yandex.ru>
2024-03-14 13:11:33 +0300

    feat: add feedback component

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Feedback.css';
+
+
+function Feedback({avatar, name, text}) {
+  return (
+    <div className='feedback__container'>
+      <img src={avatar} alt='Avatar' className='feedback__avatar'></img>
+      <div className='feedback__author-content'>
+        <p className='feedback__author-name'>{name}</p>
+        <p className='feedback__author-feedback'>
+          <span className='feedback__author-feedback-text'>
+            {text}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Feedback;
+
+Feedback.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
