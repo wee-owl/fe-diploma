d4ba03722756b4822a222e03377f01afae502a2c
wee-owl <kem-cv@yandex.ru>
2024-03-21 17:10:48 +0300

    feat: add calendar component

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+import './Calendar.css';
+
+
+function Calendar({name}) {
+  let $input = useRef();
+  let element = useRef();
+
+  useEffect(() => {
+    element.current = new AirDatepicker($input.current, {
+      navTitles: {
+        days: 'MMMM',
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    element.current.update({});
+  }, []);
+
+
+  return (
+    <input 
+      ref={$input} 
+      placeholder="ДД/ММ/ГГ" 
+      className={`fieldset__input ${name}`} 
+      required 
+    />
+  );
+}
+
+export default Calendar;
+
+Calendar.propTypes = {
+  name: PropTypes.string.isRequired,
+};
