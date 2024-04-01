5b3465496c7cb2aaac8839cf3fcbb07955d42ba0
wee-owl <kem-cv@yandex.ru>
2024-04-01 17:59:39 +0300

    feat: finish page dev

---
+++ src/pages/FinishPage/FinishPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/HeaderFinal/HeaderFinal';
+import Footer from '../Footer/Footer';
+import './FinishPage.css';
+
+
+function FinishPage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default FinishPage;
