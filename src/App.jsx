3a1ba7a8e065a5f558de4770220100713e79145d
wee-owl <kem-cv@yandex.ru>
2024-04-01 18:01:31 +0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-// import { Routes, Route } from "react-router-dom";
-import Header from './pages/Header/Header';
-import Main from './pages/Main/Main';
-import Footer from './pages/Footer/Footer';
+import { Routes, Route } from "react-router-dom";
+import StartPage from './pages/StartPage/StartPage';
+import OrderPage from './pages/OrderPage/OrderPage';
+import FinishPage from './pages/FinishPage/FinishPage';
 import './App.css';
 
 
 function App() {
   return (
     <div className='app'>
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route path='/fe-diploma' Component={StartPage} />
+        <Route path='/fe-diploma/order' Component={OrderPage} />
+        <Route path='/fe-diploma/finish' Component={FinishPage} />
+      </Routes>
     </div>
   );
 }
